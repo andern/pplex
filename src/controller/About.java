2dbb8f0e553cd7d71951461ef713f24dc2ce6584
andern <andern@gmail.com>
2013-04-21 10:36:24 +0200

    Removed unused imports.

--- src/controller/About.java
+++ src/controller/About.java
@@ -18,9 +18,6 @@
  */
 package controller;
 
-import java.awt.Color;
-import java.awt.Component;
-import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
